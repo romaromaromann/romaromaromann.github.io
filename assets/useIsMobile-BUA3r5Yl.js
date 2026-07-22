5be21cec23059f02fd811566dd63a9a9f69ce41b
romaromaromann <innirvana96@gmail.com>
2026-07-22 10:30:36 +0400

    front update

---
+++ assets/useIsMobile-BUA3r5Yl.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-B2t7Hm1D.js";const n=(n=1200)=>{const[t,r]=e.useState(window.innerWidth<n);return e.useEffect(()=>{const e=()=>r(window.innerWidth<n);return window.addEventListener("resize",e),()=>window.removeEventListener("resize",e)},[n]),t};export{n as u};
